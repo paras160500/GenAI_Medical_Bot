c6884a370348c6d82aab29b4144c83d5074af016
paras160500 <paraspatel160500@gmail.com>
2026-05-18 16:31:42 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup 
+
+setup(
+    name = "medical_chat_bot",
+    version = "0.1.0",
+    author = "Paras Patel",
+    author_email = "paraspatel160500@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
